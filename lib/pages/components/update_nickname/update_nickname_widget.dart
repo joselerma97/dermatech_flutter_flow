a096aa5ff02a3cfca6124c93209f997460248f56
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-11 09:09:46 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/pages/components/update_nickname/update_nickname_widget.dart
@@ -0,0 +1,189 @@
+import '/backend/api_requests/api_calls.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
+import 'update_nickname_model.dart';
+export 'update_nickname_model.dart';
+
+class UpdateNicknameWidget extends StatefulWidget {
+  const UpdateNicknameWidget({super.key});
+
+  @override
+  State<UpdateNicknameWidget> createState() => _UpdateNicknameWidgetState();
+}
+
+class _UpdateNicknameWidgetState extends State<UpdateNicknameWidget> {
+  late UpdateNicknameModel _model;
+
+  @override
+  void setState(VoidCallback callback) {
+    super.setState(callback);
+    _model.onUpdate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => UpdateNicknameModel());
+
+    _model.newNicknameController ??= TextEditingController();
+    _model.newNicknameFocusNode ??= FocusNode();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _model.maybeDispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    context.watch<FFAppState>();
+
+    return Card(
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      color: FlutterFlowTheme.of(context).secondaryBackground,
+      elevation: 4.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            'Update Nickname',
+            textAlign: TextAlign.center,
+            style: FlutterFlowTheme.of(context).bodyMedium,
+          ),
+          Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(8.0, 0.0, 8.0, 0.0),
+            child: TextFormField(
+              controller: _model.newNicknameController,
+              focusNode: _model.newNicknameFocusNode,
+              autofocus: true,
+              obscureText: false,
+              decoration: InputDecoration(
+                labelText: 'New Nickname...',
+                labelStyle: FlutterFlowTheme.of(context).labelMedium,
+                hintStyle: FlutterFlowTheme.of(context).labelMedium,
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: FlutterFlowTheme.of(context).alternate,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: FlutterFlowTheme.of(context).primary,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                errorBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: FlutterFlowTheme.of(context).error,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                focusedErrorBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: FlutterFlowTheme.of(context).error,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                prefixIcon: const Icon(
+                  Icons.edit,
+                ),
+              ),
+              style: FlutterFlowTheme.of(context).bodyMedium,
+              validator:
+                  _model.newNicknameControllerValidator.asValidator(context),
+            ),
+          ),
+          if (_model.isLoading)
+            Lottie.asset(
+              'assets/lottie_animations/loading_animation.json',
+              width: 30.0,
+              height: 40.0,
+              fit: BoxFit.fitHeight,
+              animate: true,
+            ),
+          FFButtonWidget(
+            onPressed: () async {
+              if (_model.newNicknameController.text == '') {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      'Server Error',
+                      style: TextStyle(
+                        color: FlutterFlowTheme.of(context).primaryText,
+                      ),
+                    ),
+                    duration: const Duration(milliseconds: 4000),
+                    backgroundColor: FlutterFlowTheme.of(context).secondary,
+                  ),
+                );
+              } else {
+                setState(() {
+                  _model.isLoading = true;
+                });
+                _model.nicknameUpdate = await UpdateNicknameCall.call(
+                  userId: FFAppState().userIdServer,
+                  nickname: _model.newNicknameController.text,
+                );
+                if ((_model.nicknameUpdate?.succeeded ?? true)) {
+                  setState(() {
+                    _model.isLoading = false;
+                  });
+                  Navigator.pop(context, true);
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        'Server Error',
+                        style: TextStyle(
+                          color: FlutterFlowTheme.of(context).primaryText,
+                        ),
+                      ),
+                      duration: const Duration(milliseconds: 4000),
+                      backgroundColor: FlutterFlowTheme.of(context).secondary,
+                    ),
+                  );
+                }
+              }
+
+              setState(() {});
+            },
+            text: 'Update',
+            options: FFButtonOptions(
+              height: 40.0,
+              padding: const EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0),
+              iconPadding: const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+              color: FlutterFlowTheme.of(context).primary,
+              textStyle: FlutterFlowTheme.of(context).titleSmall.override(
+                    fontFamily: 'Readex Pro',
+                    color: Colors.white,
+                  ),
+              elevation: 3.0,
+              borderSide: const BorderSide(
+                color: Colors.transparent,
+                width: 1.0,
+              ),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
