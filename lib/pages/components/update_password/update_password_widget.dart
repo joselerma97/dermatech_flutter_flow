ecb888a2fbf2afecea38211ecf33f8dd097a394a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-11 09:33:11 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/pages/components/update_password/update_password_widget.dart
@@ -0,0 +1,358 @@
+import '/backend/api_requests/api_calls.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
+import 'update_password_model.dart';
+export 'update_password_model.dart';
+
+class UpdatePasswordWidget extends StatefulWidget {
+  const UpdatePasswordWidget({super.key});
+
+  @override
+  State<UpdatePasswordWidget> createState() => _UpdatePasswordWidgetState();
+}
+
+class _UpdatePasswordWidgetState extends State<UpdatePasswordWidget> {
+  late UpdatePasswordModel _model;
+
+  @override
+  void setState(VoidCallback callback) {
+    super.setState(callback);
+    _model.onUpdate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => UpdatePasswordModel());
+
+    _model.passwordOldController ??= TextEditingController();
+    _model.passwordOldFocusNode ??= FocusNode();
+
+    _model.passwordNewController ??= TextEditingController();
+    _model.passwordNewFocusNode ??= FocusNode();
+
+    _model.passwordNew2Controller ??= TextEditingController();
+    _model.passwordNew2FocusNode ??= FocusNode();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _model.maybeDispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    context.watch<FFAppState>();
+
+    return Card(
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      color: FlutterFlowTheme.of(context).secondaryBackground,
+      elevation: 4.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            'Update Password',
+            textAlign: TextAlign.center,
+            style: FlutterFlowTheme.of(context).bodyMedium,
+          ),
+          Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(8.0, 0.0, 8.0, 0.0),
+            child: TextFormField(
+              controller: _model.passwordOldController,
+              focusNode: _model.passwordOldFocusNode,
+              autofocus: true,
+              textInputAction: TextInputAction.next,
+              obscureText: !_model.passwordOldVisibility,
+              decoration: InputDecoration(
+                labelText: 'Old Password...',
+                labelStyle: FlutterFlowTheme.of(context).labelMedium,
+                hintStyle: FlutterFlowTheme.of(context).labelMedium,
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: FlutterFlowTheme.of(context).alternate,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: FlutterFlowTheme.of(context).primary,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                errorBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: FlutterFlowTheme.of(context).error,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                focusedErrorBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: FlutterFlowTheme.of(context).error,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                prefixIcon: const Icon(
+                  Icons.mode_sharp,
+                ),
+                suffixIcon: InkWell(
+                  onTap: () => setState(
+                    () => _model.passwordOldVisibility =
+                        !_model.passwordOldVisibility,
+                  ),
+                  focusNode: FocusNode(skipTraversal: true),
+                  child: Icon(
+                    _model.passwordOldVisibility
+                        ? Icons.visibility_outlined
+                        : Icons.visibility_off_outlined,
+                    size: 22,
+                  ),
+                ),
+              ),
+              style: FlutterFlowTheme.of(context).bodyMedium,
+              validator:
+                  _model.passwordOldControllerValidator.asValidator(context),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(8.0, 0.0, 8.0, 0.0),
+            child: TextFormField(
+              controller: _model.passwordNewController,
+              focusNode: _model.passwordNewFocusNode,
+              autofocus: true,
+              textInputAction: TextInputAction.next,
+              obscureText: !_model.passwordNewVisibility,
+              decoration: InputDecoration(
+                labelText: 'Label here...',
+                labelStyle: FlutterFlowTheme.of(context).labelMedium,
+                hintStyle: FlutterFlowTheme.of(context).labelMedium,
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: FlutterFlowTheme.of(context).alternate,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: FlutterFlowTheme.of(context).primary,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                errorBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: FlutterFlowTheme.of(context).error,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                focusedErrorBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: FlutterFlowTheme.of(context).error,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                prefixIcon: const Icon(
+                  Icons.update,
+                ),
+                suffixIcon: InkWell(
+                  onTap: () => setState(
+                    () => _model.passwordNewVisibility =
+                        !_model.passwordNewVisibility,
+                  ),
+                  focusNode: FocusNode(skipTraversal: true),
+                  child: Icon(
+                    _model.passwordNewVisibility
+                        ? Icons.visibility_outlined
+                        : Icons.visibility_off_outlined,
+                    size: 22,
+                  ),
+                ),
+              ),
+              style: FlutterFlowTheme.of(context).bodyMedium,
+              validator:
+                  _model.passwordNewControllerValidator.asValidator(context),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(8.0, 0.0, 8.0, 0.0),
+            child: TextFormField(
+              controller: _model.passwordNew2Controller,
+              focusNode: _model.passwordNew2FocusNode,
+              autofocus: true,
+              textInputAction: TextInputAction.done,
+              obscureText: !_model.passwordNew2Visibility,
+              decoration: InputDecoration(
+                labelText: 'Label here...',
+                labelStyle: FlutterFlowTheme.of(context).labelMedium,
+                hintStyle: FlutterFlowTheme.of(context).labelMedium,
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: FlutterFlowTheme.of(context).alternate,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: FlutterFlowTheme.of(context).primary,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                errorBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: FlutterFlowTheme.of(context).error,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                focusedErrorBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: FlutterFlowTheme.of(context).error,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                prefixIcon: const Icon(
+                  Icons.update,
+                ),
+                suffixIcon: InkWell(
+                  onTap: () => setState(
+                    () => _model.passwordNew2Visibility =
+                        !_model.passwordNew2Visibility,
+                  ),
+                  focusNode: FocusNode(skipTraversal: true),
+                  child: Icon(
+                    _model.passwordNew2Visibility
+                        ? Icons.visibility_outlined
+                        : Icons.visibility_off_outlined,
+                    size: 22,
+                  ),
+                ),
+              ),
+              style: FlutterFlowTheme.of(context).bodyMedium,
+              validator:
+                  _model.passwordNew2ControllerValidator.asValidator(context),
+            ),
+          ),
+          if (_model.isLoading)
+            Lottie.network(
+              'https://assets2.lottiefiles.com/packages/lf20_aZTdD5.json',
+              width: 150.0,
+              height: 60.0,
+              fit: BoxFit.fitHeight,
+              animate: true,
+            ),
+          FFButtonWidget(
+            onPressed: () async {
+              if ((_model.passwordOldController.text == '') &&
+                  (_model.passwordNewController.text == '') &&
+                  (_model.passwordNew2Controller.text == '')) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      'Fill all fields',
+                      style: TextStyle(
+                        color: FlutterFlowTheme.of(context).primaryText,
+                      ),
+                    ),
+                    duration: const Duration(milliseconds: 4000),
+                    backgroundColor: FlutterFlowTheme.of(context).secondary,
+                  ),
+                );
+              } else {
+                if (_model.passwordNewController.text ==
+                    _model.passwordNew2Controller.text) {
+                  setState(() {
+                    _model.isLoading = true;
+                  });
+                  _model.updatePassswordResult =
+                      await UpdateUserPasswordCall.call(
+                    userId: FFAppState().userIdServer,
+                    passwordOld: _model.passwordOldController.text,
+                    passwordNew: _model.passwordNewController.text,
+                  );
+                  if ((_model.updatePassswordResult?.succeeded ?? true)) {
+                    Navigator.pop(
+                        context,
+                        UpdateUserPasswordCall.status(
+                          (_model.updatePassswordResult?.jsonBody ?? ''),
+                        ));
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          'Server Error',
+                          style: TextStyle(
+                            color: FlutterFlowTheme.of(context).primaryText,
+                          ),
+                        ),
+                        duration: const Duration(milliseconds: 4000),
+                        backgroundColor: FlutterFlowTheme.of(context).secondary,
+                      ),
+                    );
+                  }
+
+                  setState(() {
+                    _model.isLoading = false;
+                  });
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        'Passwords do not match',
+                        style: TextStyle(
+                          color: FlutterFlowTheme.of(context).primaryText,
+                        ),
+                      ),
+                      duration: const Duration(milliseconds: 4000),
+                      backgroundColor: FlutterFlowTheme.of(context).secondary,
+                    ),
+                  );
+                }
+              }
+
+              setState(() {});
+            },
+            text: 'Update Password',
+            options: FFButtonOptions(
+              height: 40.0,
+              padding: const EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0),
+              iconPadding: const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+              color: FlutterFlowTheme.of(context).primary,
+              textStyle: FlutterFlowTheme.of(context).titleSmall.override(
+                    fontFamily: 'Readex Pro',
+                    color: Colors.white,
+                  ),
+              elevation: 3.0,
+              borderSide: const BorderSide(
+                color: Colors.transparent,
+                width: 1.0,
+              ),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
